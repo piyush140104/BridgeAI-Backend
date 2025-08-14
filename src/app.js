9ddce9a014ed0bf8874eb4ca6f35442ff1b478f9
Piyush Gupta <piyushgupta@192.168.1.30>
2025-08-15 02:34:27 +0530

    remove nodemon and again deploy

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import session from "express-session";
 
-require('dotenv').config();
 const app = express();
 
 const allowedOrigins = [
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import express from "express";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-require('dotenv').config();
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
